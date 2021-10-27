c19d529bfda94fc5b7ce594c1fd695274a7827eb
Thiago Amaral <thiago.amaral@outlook.com>
2021-10-27 15:44:11 -0300

    getting response back

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -2,6 +2,7 @@
   <v-container>
     <v-row align="center">
       <v-col cols="12">
+        <br />
         <v-text center
           >Select a Star Wars Character, and see which movies it's been
           in!</v-text
@@ -11,34 +12,62 @@
     <v-row align="center">
       <v-col cols="12">
         <v-autocomplete
+          @change="getCharMovies"
           v-model="value"
-          :items="items"
-          dense
+          :items="charNames"
           filled
           dark
           label="Search or select character"
         ></v-autocomplete>
+        <!-- #! BEGIN DELETE -->
+        <!-- <v-text>{{ characters }}</v-text
+        ><br /> -->
+        <v-text>{{ charNames }}</v-text
+        ><br />
+        <v-text>{{ charMovies }}</v-text
+        ><br />
+        <!-- #! END DELETE -->
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "HelloWorld",
 
   data: () => ({
-    items: ["foo", "bar", "fizz", "buzz"],
-    values: ["foo", "bar"],
+    response: [],
+    results: [],
+    charNames: [],
+    charMovies: [],
     value: null,
   }),
+  methods: {
+    async getAllChars() {
+      this.response = await axios
+        .get("https://swapi.dev/api/people/")
+        .then((response) => (this.results = response.data.results));
+
+      for (let result in this.results) {
+        this.charNames.push(result.name);
+      }
+    },
+    // getCharMovies() {
+    //   alert("getCharMovies");
+    // },
+  },
+  mounted() {
+    this.getAllChars();
+  },
 };
 </script>
 
 <style scoped>
 v-text {
   color: rgb(255, 255, 255);
-  padding-top: 100%;
   text-align: center;
   font-size: 20px;
 }
