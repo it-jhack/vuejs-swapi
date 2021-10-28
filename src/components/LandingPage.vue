783fcddbe84f53192c84e49e0de1c32f7657477a
Thiago Amaral <thiago.amaral@outlook.com>
2021-10-28 17:38:26 -0300

    code cleaning

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
-    <v-row align="center">
-      <v-col cols="12">
+    <v-row>
+      <v-col cols="10" class="center">
         <br />
         <v-text center
           >Search or select a Star Wars character, and see which movies it's
@@ -10,8 +10,8 @@
       </v-col>
     </v-row>
 
-    <v-row align="center">
-      <v-col cols="12">
+    <v-row>
+      <v-col cols="10" class="center">
         <v-autocomplete
           @change="getCharMovies"
           v-model="value"
@@ -20,45 +20,13 @@
           filled
           dark
           label="Search or select character"
+          hide-no-data
         ></v-autocomplete>
-        <!-- #! BEGIN DELETE -->
-        <v-expansion-panels dark multiple>
-          <v-expansion-panel>
-            <v-expansion-panel-header> response </v-expansion-panel-header>
-            <v-expansion-panel-content>
-              {{ response }}
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-
-          <v-expansion-panel>
-            <v-expansion-panel-header> characters </v-expansion-panel-header>
-            <v-expansion-panel-content>
-              {{ characters }}
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-
-          <v-expansion-panel>
-            <v-expansion-panel-header>
-              charMoviesApiUrl
-            </v-expansion-panel-header>
-            <v-expansion-panel-content>
-              {{ charMoviesApiUrl }}
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-
-          <v-expansion-panel>
-            <v-expansion-panel-header> moviesData </v-expansion-panel-header>
-            <v-expansion-panel-content>
-              {{ moviesData }}
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-expansion-panels>
-        <!-- #! END DELETE -->
       </v-col>
     </v-row>
 
     <v-row>
-      <v-col cols="8">
+      <v-col cols="10" class="center">
         <h2 v-if="value != null">Movies that {{ value }} participated:</h2>
         <v-data-table
           v-if="value != null"
@@ -70,12 +38,6 @@
           item-key="name"
           class="elevation-1"
         ></v-data-table>
-      </v-col>
-
-      <!-- #! TODO -->
-      <v-col cols="4">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero,
-        pariatur.
       </v-col>
     </v-row>
   </v-container>
@@ -138,6 +100,8 @@ export default {
           this.charMoviesApiUrl = char.films;
         }
       });
+
+      // Request each movie url listed under selected char
       this.charMoviesApiUrl.forEach((movieApiUrl) => {
         axios.get(movieApiUrl).then((response) =>
           this.moviesData.push({
@@ -155,6 +119,7 @@ export default {
       return [p[2], p[1], p[0]].join("/");
     },
   },
+
   mounted() {
     this.getAllChars();
   },
@@ -170,5 +135,10 @@ v-text {
 
 h2 {
   color: rgb(255, 255, 255);
+}
+
+.center {
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
