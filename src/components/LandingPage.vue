61ba4aea273dccc7f60f88c5026555e7f42e9e1b
Thiago Amaral <thiago.amaral@outlook.com>
2021-10-28 07:30:01 -0300

    axios get movies data for each char

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -9,12 +9,14 @@
         >
       </v-col>
     </v-row>
+
     <v-row align="center">
       <v-col cols="12">
         <v-autocomplete
           @change="getCharMovies"
           v-model="value"
-          :items="response.data.results.name"
+          :items="response.data.results"
+          item-text=".name"
           filled
           dark
           label="Search or select character"
@@ -29,20 +31,38 @@
           </v-expansion-panel>
 
           <v-expansion-panel>
-            <v-expansion-panel-header> charNames </v-expansion-panel-header>
+            <v-expansion-panel-header>
+              charMoviesApiUrl
+            </v-expansion-panel-header>
             <v-expansion-panel-content>
-              {{ charNames }}
+              {{ charMoviesApiUrl }}
             </v-expansion-panel-content>
           </v-expansion-panel>
 
           <v-expansion-panel>
-            <v-expansion-panel-header> charMovies </v-expansion-panel-header>
+            <v-expansion-panel-header> moviesData </v-expansion-panel-header>
             <v-expansion-panel-content>
-              {{ charMovies }}
+              {{ moviesData }}
             </v-expansion-panel-content>
           </v-expansion-panel>
         </v-expansion-panels>
         <!-- #! END DELETE -->
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="8">
+        <h1 v-if="value != null">Movies that {{ value }} participated:</h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium
+        accusamus blanditiis, beatae dolore consectetur, impedit saepe porro ex
+        voluptatibus provident fugiat velit. Necessitatibus autem quod neque
+        quae porro, impedit unde maxime ex eius expedita? Id, illum repellendus?
+        Nam excepturi molestiae asperiores consequuntur, consequatur incidunt
+        neque debitis suscipit quasi praesentium architecto?
+      </v-col>
+      <v-col cols="4">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero,
+        pariatur.
       </v-col>
     </v-row>
   </v-container>
@@ -56,17 +76,27 @@ export default {
 
   data: () => ({
     response: [],
-    charNames: [],
-    charMovies: [],
+    charMoviesApiUrl: null,
+    moviesData: [],
     value: null,
   }),
   methods: {
     async getAllChars() {
       this.response = await axios.get("https://swapi.dev/api/people/");
+    },
 
+    getCharMovies() {
       this.response.data.results.forEach((result) => {
-        this.charNames.push(result.name);
-        // let index = result.
+        if (result.name == this.value) {
+          this.charMoviesApiUrl = result.films;
+        }
+      });
+      this.charMoviesApiUrl.forEach((movieApiUrl) => {
+        // this.moviesData.push(axios.get(movieApiUrl));
+        // this.moviesData.push(async await axios.get("https://swapi.dev/api/people/"));
+        axios
+          .get(movieApiUrl)
+          .then((response) => this.moviesData.push(response));
       });
     },
   },
