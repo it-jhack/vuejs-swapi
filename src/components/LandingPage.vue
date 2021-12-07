18f90cccbd1f4a34b7b6d843b40a5f2b8dfefd04
Thiago Amaral <thiago.amaral@outlook.com>
2021-12-07 20:19:34 -0300

    add char image search using search-engine api

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -8,28 +8,66 @@
           been in!</v-text
         >
       </v-col>
+      <br /><br />
     </v-row>
 
     <v-row>
       <v-col cols="10" class="center">
         <v-autocomplete
           @change="getCharMovies()"
-          v-model="value"
+          v-model="selectedCharName"
           :items="characters"
           item-text=".name"
           filled
           dark
           label="Search or select character"
           hide-no-data
-        ></v-autocomplete>
+        >
+        </v-autocomplete>
+      </v-col>
+    </v-row>
+
+    <v-row justify="center">
+      <v-col cols="7" order="1">
+        <!-- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit tempora
+        accusamus neque laboriosam accusantium eaque recusandae asperiores
+        mollitia placeat culpa, rerum perspiciatis! Autem doloribus voluptates
+        dignissimos atque blanditiis? Magnam quod rem aperiam corrupti atque
+        blanditiis nihil mollitia. Libero minus nihil nobis, quidem perspiciatis
+        sed error tenetur et obcaecati accusamus. Expedita!
+
+        <br /><br />
+
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit tempora
+        accusamus neque laboriosam accusantium eaque recusandae asperiores
+        mollitia placeat culpa, rerum perspiciatis! Autem doloribus voluptates
+        dignissimos atque blanditiis? Magnam quod rem aperiam corrupti atque
+        blanditiis nihil mollitia. Libero minus nihil nobis, quidem perspiciatis
+        sed error tenetur et obcaecati accusamus. Expedita! -->
+      </v-col>
+      <v-col cols="3" align-self="start" order="0" v-if="!isMobile()">
+        <v-img
+          :src="charImgUrl"
+          lazy-src="src/assets/unknown-char.png"
+          v-if="charImgUrl"
+        >
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular indeterminate color="grey lighten-5">
+              </v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
       </v-col>
     </v-row>
 
     <v-row>
-      <v-col cols="10" class="center">
-        <h2 v-if="value != null">Movies that {{ value }} participated:</h2>
+      <v-col cols="10" class="center" v-if="!isMobile()">
+        <h2 v-if="selectedCharName != null">
+          Movies that {{ selectedCharName }} participated in:
+        </h2>
         <v-data-table
-          v-if="value != null"
+          v-if="selectedCharName != null"
           hide-default-footer
           dark
           dense
@@ -38,7 +76,7 @@
           :sort-by.sync="sortBy"
           :sort-desc.sync="sortDesc"
           must-sort
-          :headers="headers"
+          :headers="tbHeaders"
           :items="moviesData"
           item-key="name"
           class="elevation-1"
@@ -49,6 +87,7 @@
         </v-data-table>
       </v-col>
     </v-row>
+    <br /><br />
   </v-container>
 </template>
 
@@ -62,10 +101,30 @@ export default {
     charMoviesApiUrl: null,
     moviesData: [],
     loadingTable: true,
-    value: null,
+    selectedCharName: null,
     sortBy: "release_date",
     sortDesc: false,
-    headers: [
+
+    // Api keys:
+    VUE_APP_AZURE_SUBSCRIPTION_KEY: process.env.VUE_APP_AZURE_SUBSCRIPTION_KEY,
+    // How to get one: https://docs.microsoft.com/en-us/bing/search-apis/bing-custom-search/how-to/quick-start
+    VUE_APP_CUSTOM_CONFIG: process.env.VUE_APP_CUSTOM_CONFIG,
+    // How to get one: https://docs.microsoft.com/en-us/azure/cognitive-services/bing-custom-search/quick-start
+
+    // Remember to set your api keys in a '.env.local' file inside the project;
+    // See: https://cli.vuejs.org/guide/mode-and-env.html#modes and https://youtu.be/XIptuxLGDyk
+    // You should never hardcode or upload api keys to repositories (even private ones), as that's a critical
+    // security risk.
+
+    // To safely deploy them on simple live applications:
+    // https://www.freecodecamp.org/news/private-api-keys/
+
+    count: 1,
+    mkt: "en-US",
+    imgSearchResponse: null,
+    charImgUrl: null,
+
+    tbHeaders: [
       {
         text: "Film Title",
         align: "start",
@@ -76,6 +135,7 @@ export default {
       { text: "Release Date (year-month-day)", value: "release_date" },
     ],
   }),
+
   methods: {
     displayDate(dateString) {
       let p = dateString.split(/\D/g);
@@ -107,11 +167,13 @@ export default {
     },
 
     async getCharMovies() {
+      this.bingWebSearch("freddy mercury success meme");
+
       this.loadingTable = true;
       this.moviesData = []; //resetting array, so it does not pile up with previous selections
 
       this.characters.forEach((char) => {
-        if (char.name == this.value) {
+        if (char.name == this.selectedCharName) {
           this.charMoviesApiUrl = char.films;
         }
       });
@@ -128,6 +190,60 @@ export default {
         );
       });
       this.loadingTable = "false";
+    },
+
+    bingWebSearch() {
+      const https = require("https");
+
+      this.imgSearchResponse = https.get(
+        {
+          hostname: "api.bing.microsoft.com",
+          path:
+            "/v7.0/custom/images/search?q=" +
+            encodeURIComponent(this.selectedCharName) +
+            "&count=" +
+            this.count +
+            "&customConfig=" +
+            this.VUE_APP_CUSTOM_CONFIG +
+            "&mkt=" +
+            this.mkt,
+          headers: {
+            "Ocp-Apim-Subscription-Key": this.VUE_APP_AZURE_SUBSCRIPTION_KEY,
+          },
+        },
+        (res) => {
+          let body = "";
+          res.on("data", (part) => (body += part));
+          res.on("end", () => {
+            for (var header in res.headers) {
+              if (
+                header.startsWith("bingapis-") ||
+                header.startsWith("x-msedge-")
+              ) {
+                console.log(header + ": " + res.headers[header]);
+              }
+            }
+            let responseObj = JSON.parse(body);
+            this.charImgUrl = responseObj.value[0].thumbnailUrl;
+          });
+          res.on("error", (e) => {
+            console.log("Error: " + e.message);
+            throw e;
+          });
+        }
+      );
+    },
+
+    isMobile() {
+      if (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        )
+      ) {
+        return true;
+      } else {
+        return false;
+      }
     },
   },
 
